d3b6c682610c8449d70bb0014376eb7ace1992ce
Baccanelli Marco <d7035@italiaonline.it>
2018-11-07 15:22:52 +0100

    Aggiunto evento di connessione e disconnessione

--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
 const path = require('path');
+const http = require('http');
+const exp = require('express');
+const socketIO = require('socket.io');
+
+
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
-const exp = require('express');
 
 
-var app = exp();
+var app = exp();											//applicazione express
+var server = http.createServer(app);						//creo un server utilizzando il modulo http nativo di node
+var io = socketIO(server);									//in questo modo configuro il server per funzionare con il web socket
+
+
 app.use(exp.static(publicPath));
 
-app.listen(port, () => {
-	console.log(`Avviata sulla porta ${port}`);
+io.on('connection', (socket) => {							//registro l'evento (connection) e gli passo l'oggetto socket definito dentro il client
+	console.log('Nuovo utente collegato');
+
+
+	socket.on('disconnect', (soket) => {
+		console.log('Utente scollegato');
+	})
 });
 
 
-module.exports = {app};
+server.listen(port, () => {
+	console.log(`Avviata sulla porta ${port}`);
+});
